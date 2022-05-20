a7e300ef4e326b3a2fdc5772db8300e7a84f776a
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-20 12:08:40 +0200

    add auth functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,36 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 import { NavLink } from "react-router-dom";
 
 
-function NavBar () {
+function NavBar() {
+    const { isLoggedIn, isLoading, user, logOutUser } = useContext(AuthContext);
 
-    
     return (
         <div className="sidebar">
             <h1>BudgetManager</h1>
+            {!isLoggedIn &&
+                <>
+                    <NavLink to="/signup">Register</NavLink> |
+                    <NavLink to="/login">Login</NavLink>
+                </>
+            }
+            {isLoggedIn &&
+                <>
+                    <h3>Welcome, {user.email}!</h3>
+                    <button onClick={logOutUser}>Logout</button>
 
-            <div>
-            <h3>Dashboard</h3>
-            <NavLink to="/userprofile">User Profile</NavLink> <br/>
-            <button>Logout</button>
-            </div>
+                    <div>
+                        <NavLink to="/dashboard">Dashboard</NavLink> <br />
+                        <NavLink to="/userprofile">User Profile</NavLink> <br />
+                    </div>
 
-            <div>
-            <h3>Statement</h3>
-            <NavLink to="#">+New</NavLink>
-            </div>
+                    <div>
+                        <NavLink to="/statements">Statements</NavLink> <br />
+                        <NavLink to="/statements/create">+New</NavLink>
+                    </div>
+                </>
+            }
 
 
 
