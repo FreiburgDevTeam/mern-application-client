7d5731bfbd6312271127343eb3da5c3b87eca7d4
Alexander <alexander.nerz@web.de>
2022-05-21 13:42:39 +0200

    get statements in Dashboard

--- src/components/StatementList.js
+++ src/components/StatementList.js
@@ -1,8 +1,33 @@
-function StatementList() {
+import Spinner  from "reactjs-simple-spinner";
+
+function StatementList(props) {
+
+    const renderStatements = () => {
+        const list = props.statements.map ( (e) => {
+            return (
+                <div key={e._id}>
+                            ||
+                    <h5>{e.title}</h5>
+                    <p>{e.amount}</p>
+                    <p>{e.type}</p> 
+                </div>
+            )
+        })
+        return list;
+    }
+
 
     return(
         <>
-            <h1>This is StatementLis Component</h1>
+            <h1>This is StatementList Component</h1>
+            <div>
+                {props.statements === null
+                    ? <Spinner />
+                    : renderStatements()
+                }
+
+            </div>
+
         </>
     )
 }
