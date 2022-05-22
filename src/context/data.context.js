2694be88488e1c0fdfada83d78f7ea889d9cd6e0
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-22 11:34:38 +0200

    update data context, execute only if user is logged

--- src/context/data.context.js
+++ src/context/data.context.js
@@ -1,16 +1,22 @@
 import axios from "axios";
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
+import { AuthContext } from "./auth.context";
 
 const DataContext = createContext();
 
+
 function DataProviderWrapper(props) {
     const [statements, setStatements] = useState(null);
+    const { isLoggedIn } = useContext(AuthContext);
 
     useEffect(() => {
+        if (isLoggedIn) {
         updateData()
-    }, [])
+    }
+    }, [isLoggedIn])
 
     const updateData = () => {
+        
         const storedToken = localStorage.getItem("authToken");
         axios.get(`${process.env.REACT_APP_API_URL}/statements`,
             { headers: { Authorization: `Bearer ${storedToken}` } }
@@ -20,6 +26,7 @@ function DataProviderWrapper(props) {
                 setStatements(response.data)
             })
             .catch(e => console.log("error getting projects from API...", e));
+        
     }
 
     return (
