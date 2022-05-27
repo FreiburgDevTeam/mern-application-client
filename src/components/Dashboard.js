6ca8f458afff841b3a21a518048bd968a77aad94
Oscar Gonzalez <oscargonzalezpena@gmail.com>
2022-05-27 10:17:57 +0200

    remove unncessary title

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,12 @@
 import Balance from "./Balance";
 import StatementList from "./StatementList";
 import StatementsChart from "./StatementsChart";
-import Title from "./Title";
 
 function Dashboard() {
     return (
         <>
             {
                 <div>
-                    <Title>Dashboard</Title><br />
                         <div className="flex-row head-container">
                             <Balance />
                             <StatementsChart />
